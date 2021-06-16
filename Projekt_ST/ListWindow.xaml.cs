990ed637a0be7062e64d1d6579a887448039575f
Krzysztof Bazylak <bazylov@gmail.com>
2021-06-16 21:20:58 +0200

    Add project files.

---
+++ Projekt_ST/ListWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt_ST
+{
+    /// <summary>
+    /// Interaction logic for ListWindow.xaml
+    /// </summary>
+    public partial class ListWindow : Window
+    {
+        ObservableCollection<Student> ListaStudentow;
+        public ListWindow()
+        {
+            InitializeComponent();
+             ListaStudentow = new ObservableCollection<Student>
+            {
+                new Student(1, 1, "Adam", "Kowal", "123456789", 'm', "Wesola", "345", "Rzeszow"),
+                new Student(1, 1, "Jan", "Kowalski", "123456789", 'm', "Mickiewicza", "123", "Rzeszow"),
+            };
+            txtTest.Text = ListaStudentow[0].phone.ToString();
+            oknoListaStudentow.ItemsSource = ListaStudentow;
+        }
+
+        private void FiltrButton_Click(object sender, RoutedEventArgs e)
+        {
+           // ObservableCollection<Student> FiltrLista = new ObservableCollection<Student>
+        }
+    }
+}
---
+++ Projekt_ST/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_ST
+{
+    public class Student
+    {
+        
+        public Student(int stuentIdd, int userIdd, string imie, string nazwisko, string telefon, char plec, string ulica, string numerdomu,string miasto)
+        {
+            this.studentId = stuentIdd;
+            this.userId = userIdd;
+            this.firstName = imie;
+            this.lastName = nazwisko;
+            this.phone = telefon;
+            this.gender = plec;
+            this.addressStreet = ulica;
+            this.addressNumber = numerdomu;
+            this.city = miasto;
+        }
+        public int studentId { get; set; }
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public char gender { get; set; }
+        public string addressStreet { get; set; }
+        public string addressNumber { get; set; }
+        public string city { get; set; }
+
+
+    }
+}
